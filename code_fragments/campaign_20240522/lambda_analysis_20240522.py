e9151af7807f65702c3c8aa06825b0c8765477b3
Peter <peter.balogh.wien@gmail.com>
2025-07-15 15:38:19 +0200

    Re-did comparison of search algorithms

--- code_fragments/campaign_20240522/lambda_analysis_20240522.py
+++ code_fragments/campaign_20240522/lambda_analysis_20240522.py
@@ -1,13 +1,13 @@
 #%% 
-import TEM_tools.tem.survey_tem as st
+import TEM_tools as te
 from pathlib import Path
-from filtering_20240522 import root_path, tem_data, tem_coords, rename_points_0522, parsing_coords_0522
-
+from filtering_20240522 import root_path, tem_data, tem_coords, rename_points_0522
+from filtering_20240522 import parsing_coords_0522, erroneous_soundings_0522
 #%%
 
 if __name__ is '__main__':
     # Preprocessing
-    survey_0522 = st.SurveyTEM(
+    survey_0522 = te.SurveyTEM(
         root_path / 'data/20240522'
     )
     if Path(tem_coords).exists():
@@ -34,65 +34,87 @@ if __name__ is '__main__':
         parsing_dict=parsing_coords_0522
     )
 
-    # Good curvature: M020
-    survey_0522.lambda_analysis_comparison(
-        sounding='M020',
-        layer_type='dict',
-        layers={0:1, 5:1.5, 15:2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(8, 150),
-        fname='lambda_analysis_good_curve_20240522.png'
-    )
-    # Medium curvature: M010
-    survey_0522.lambda_analysis_comparison(
-        sounding='M010',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(8, 150),
-        fname='lambda_analysis_medium_curve_20240522.png'
-    )
-    # Small curvature: M025
-    survey_0522.lambda_analysis_comparison(
-        sounding='M025',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(8, 150),
-        fname='lambda_analysis_light_curve_20240522.png'
-    )
-    # No L-Curve: M017
-    survey_0522.lambda_analysis_comparison(
-        sounding='M017',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(8, 150),
-        fname='lambda_analysis_bad_curve_20240522.png'
-    )
+    for sounding in [f'M{i:03d}' for i in range(1, 46) if i not in erroneous_soundings_0522]:
+        _ = survey_0522.lambda_analysis_comparison(
+            sounding=sounding,
+            layer_type='dict',
+            layers={0:1, 5:1.5},
+            max_depth=20,
+            test_range=(10, 1000, 20),
+            filter_times=(8, 210),
+            fname=f'comparison_{sounding}.png'
+        )
 
-    # Sharp corner: M011
-    survey_0522.lambda_analysis_comparison(
-        sounding='M011',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(8, 150),
-        fname='lambda_analysis_sharp_curve_20240522.png'
-    )
+    for sounding in [f'M{i:03d}' for i in erroneous_soundings_0522]:
+        _ = survey_0522.lambda_analysis_comparison(
+            sounding=sounding,
+            layer_type='dict',
+            layers={0: 1, 5: 1.5},
+            max_depth=20,
+            test_range=(10, 1000, 20),
+            filter_times=(8, 210),
+            fname=f'comparison_{sounding}_err.png'
+        )
 
-    # Sharp corner with noise: M001
-    survey_0522.lambda_analysis_comparison(
-        sounding='M001',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(8, 150),
-        fname='lambda_analysis_sharp_noisy_curve_20240522.png'
-    )
+    # # Good curvature: M020
+    # survey_0522.lambda_analysis_comparison(
+    #     sounding='M020',
+    #     layer_type='dict',
+    #     layers={0:1, 5:1.5, 15:2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(8, 150),
+    #     fname='lambda_analysis_good_curve_20240522.png'
+    # )
+    # # Medium curvature: M010
+    # survey_0522.lambda_analysis_comparison(
+    #     sounding='M010',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(8, 150),
+    #     fname='lambda_analysis_medium_curve_20240522.png'
+    # )
+    # # Small curvature: M025
+    # survey_0522.lambda_analysis_comparison(
+    #     sounding='M025',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(8, 150),
+    #     fname='lambda_analysis_light_curve_20240522.png'
+    # )
+    # # No L-Curve: M017
+    # survey_0522.lambda_analysis_comparison(
+    #     sounding='M017',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(8, 150),
+    #     fname='lambda_analysis_bad_curve_20240522.png'
+    # )
+
+    # # Sharp corner: M011
+    # survey_0522.lambda_analysis_comparison(
+    #     sounding='M011',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(8, 150),
+    #     fname='lambda_analysis_sharp_curve_20240522.png'
+    # )
+
+    # # Sharp corner with noise: M001
+    # survey_0522.lambda_analysis_comparison(
+    #     sounding='M001',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(8, 150),
+    #     fname='lambda_analysis_sharp_noisy_curve_20240522.png'
+    # )
--- code_fragments/campaign_20241008/lambda_analysis_20241008.py
+++ code_fragments/campaign_20241008/lambda_analysis_20241008.py
@@ -1,7 +1,8 @@
 #%%
 import TEM_tools.tem.survey_tem as st
 from pathlib import Path
-from filtering_20241008 import root_path, tem_data, tem_coords, rename_points_1008, parsing_coords_1008
+from filtering_20241008 import root_path, tem_data, tem_coords, rename_points_1008
+from filtering_20241008 import parsing_coords_1008, erroneous_soundings_1008
 
 #%%
 
@@ -34,65 +35,88 @@ if __name__ is '__main__':
         parsing_dict=parsing_coords_1008
     )
 
-    # Good curvature: M011
-    survey_1008.lambda_analysis_comparison(
-        sounding='M011',
-        layer_type='dict',
-        layers={0:1, 5:1.5, 15:2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(12, 80),
-        fname='lambda_analysis_good_curve_20241008.png'
-    )
-    # Small curvature: M034
-    survey_1008.lambda_analysis_comparison(
-        sounding='M034',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(12, 80),
-        fname='lambda_analysis_light_curve_20241008.png'
-    )
-    # No L-Curve: M010
-    survey_1008.lambda_analysis_comparison(
-        sounding='M010',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(12, 80),
-        fname='lambda_analysis_bad_curve_20241008.png'
-    )
-    # Sharp corner: M040
-    survey_1008.lambda_analysis_comparison(
-        sounding='M040',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(12, 80),
-        fname='lambda_analysis_sharp_curve_20241008.png'
-    )
+    for sounding in [f'M{i:03d}' for i in range(1, 67) if i not in erroneous_soundings_1008]:
+        _ = survey_1008.lambda_analysis_comparison(
+            sounding=sounding,
+            layer_type='dict',
+            layers={0:1, 5:1.5},
+            max_depth=20,
+            test_range=(10, 1000, 20),
+            filter_times=(12, 80),
+            noise_floor=0.08,
+            fname=f'comparison_{sounding}.png'
+        )
 
-    # Sharp corner with noise: M038
-    survey_1008.lambda_analysis_comparison(
-        sounding='M038',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(12, 80),
-        fname='lambda_analysis_sharp_noisy_curve_20241008.png'
-    )
+    for sounding in [f'M{i:03d}' for i in erroneous_soundings_1008]:
+        _ = survey_1008.lambda_analysis_comparison(
+            sounding=sounding,
+            layer_type='dict',
+            layers={0: 1, 5: 1.5},
+            max_depth=20,
+            test_range=(10, 1000, 20),
+            filter_times=(12, 80),
+            noise_floor=0.08,
+            fname=f'comparison_{sounding}_err.png'
+        )
+    # # Good curvature: M011
+    # survey_1008.lambda_analysis_comparison(
+    #     sounding='M011',
+    #     layer_type='dict',
+    #     layers={0:1, 5:1.5, 15:2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(12, 80),
+    #     fname='lambda_analysis_good_curve_20241008.png'
+    # )
+    # # Small curvature: M034
+    # survey_1008.lambda_analysis_comparison(
+    #     sounding='M034',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(12, 80),
+    #     fname='lambda_analysis_light_curve_20241008.png'
+    # )
+    # # No L-Curve: M010
+    # survey_1008.lambda_analysis_comparison(
+    #     sounding='M010',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(12, 80),
+    #     fname='lambda_analysis_bad_curve_20241008.png'
+    # )
+    # # Sharp corner: M040
+    # survey_1008.lambda_analysis_comparison(
+    #     sounding='M040',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(12, 80),
+    #     fname='lambda_analysis_sharp_curve_20241008.png'
+    # )
 
-    # Unexpected curve: M047
-    survey_1008.lambda_analysis_comparison(
-        sounding='M047',
-        layer_type='dict',
-        layers={0: 1, 5: 1.5, 15: 2},
-        max_depth=30,
-        test_range=(10, 1000, 20),
-        filter_times=(12, 80),
-        fname='lambda_analysis_ellipse_20241008.png'
-    )
+    # # Sharp corner with noise: M038
+    # survey_1008.lambda_analysis_comparison(
+    #     sounding='M038',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(12, 80),
+    #     fname='lambda_analysis_sharp_noisy_curve_20241008.png'
+    # )
+
+    # # Unexpected curve: M047
+    # survey_1008.lambda_analysis_comparison(
+    #     sounding='M047',
+    #     layer_type='dict',
+    #     layers={0: 1, 5: 1.5, 15: 2},
+    #     max_depth=30,
+    #     test_range=(10, 1000, 20),
+    #     filter_times=(12, 80),
+    #     fname='lambda_analysis_ellipse_20241008.png'
+    # )
